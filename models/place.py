dcd57164cb3a6cafd2635d6a05bf493f168514b7
Sanaalagh <Sanaalaghzou@gmail.com>
2024-03-22 10:51:09 -0700

    updating for places

--- models/place.py
+++ models/place.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+
+# Association table for the many-to-many relationship
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey(
+                          'amenities.id'), primary_key=True, nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -18,11 +26,26 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship(
+            "Amenity", secondary=place_amenity, viewonly=False,
+            back_populates="place_amenities")
 
     # For FileStorage
     @property
+    def amenities(self):
+        """Returns the list of Amenity instances."""
+        return self.amenity_ids
+
+    @amenities.setter
+    def amenities(self, obj):
+        """Adds an Amenity.id to the amenity_ids."""
+        if type(obj).__name__ == 'Amenity':
+            self.amenity_ids.append(obj.id)
+
+    @property
     def reviews(self):
-        """Returns the list of Review instances with place_id equals to the current Place.id"""
+        """Returns the list of Review instances
+        with place_id equals to the current Place.id"""
         from models import storage
         all_reviews = storage.all(Review).values()
         return [review for review in all_reviews if review.place_id == self.id]
