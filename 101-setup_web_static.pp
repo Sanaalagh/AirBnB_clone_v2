a60fa8885aa5876d511dbf613371a25a61eca37f
Sanaalagh <sanaalaghzou@gmail.com>
2024-04-07 10:51:55 -0700

    some changes

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,51 +1,89 @@
 # 101-setup_web_static.pp
-file {
-  '/data':
-    ensure => directory,
-    owner  => 'ubuntu',
-    group  => 'ubuntu';
+# Configures a web server for deployment of web_static.
 
-  '/data/web_static':
-    ensure => directory,
-    owner  => 'ubuntu',
-    group  => 'ubuntu';
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https://th3-gr00t.tk;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
-  '/data/web_static/releases':
-    ensure => directory,
-    owner  => 'ubuntu',
-    group  => 'ubuntu';
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
-  '/data/web_static/shared':
-    ensure => directory,
-    owner  => 'ubuntu',
-    group  => 'ubuntu';
+file { '/data':
+  ensure  => 'directory'
+} ->
 
-  '/data/web_static/releases/test':
-    ensure => directory,
-    owner  => 'ubuntu',
-    group  => 'ubuntu';
+file { '/data/web_static':
+  ensure => 'directory'
+} ->
 
-  '/data/web_static/releases/test/index.html':
-    ensure  => present,
-    mode    => '0644',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    content => "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n";
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
 
-  '/data/web_static/current':
-    ensure => link,
-    target => '/data/web_static/releases/test',
-    require => File['/data/web_static/releases/test/index.html'];
+file { '/data/web_static/releases/test':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-exec { 'nginx-service-reload':
-  command     => '/usr/sbin/service nginx reload',
-  refreshonly => true,
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
 
 file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('path/to/your/template.erb'),
-  notify  => Exec['nginx-service-reload'],
-}
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
 
+exec { 'nginx restart':
+  path => '/etc/init.d/'
+}
