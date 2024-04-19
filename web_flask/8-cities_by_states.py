e93a36abd5638c5a8e92dfcadefcfe116dcf9e59
Sanaalagh <Sanaalaghzou@gmail.com>
2024-04-19 09:05:05 -0700

    commiting changes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,6 +8,7 @@ from models import storage
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/cities_by_states')
 def cities_by_states():
     """Route to display states and their associated cities."""
@@ -15,10 +16,12 @@ def cities_by_states():
     sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=sorted_states)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Teardown method to close the database session."""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
