780c7e17af55a2d0a3e5472eb8b3f0189840bfdb
Sanaalagh <Sanaalaghzou@gmail.com>
2024-04-19 09:21:12 -0700

    commiting changes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,6 +7,7 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with a list of all State objects from the storage"""
@@ -14,10 +15,12 @@ def states_list():
     states = sorted(states, key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def close_session(exception):
     """Remove the current SQLAlchemy Session."""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
