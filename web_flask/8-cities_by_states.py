32c1020e8597cdb9d59454bf94f32582a7263a75
Sanaalagh <Sanaalaghzou@gmail.com>
2024-04-21 16:35:08 -0700

    fixing key issue

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,26 +2,28 @@
 """
 Script to start a Flask web application that displays states and their cities.
 """
-from flask import Flask, render_template
 from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/cities_by_states')
+@app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Route to display states and their associated cities."""
-    states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=sorted_states)
+    """Displays an HTML page with a list of all states and related cities.
+
+    States/cities are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Teardown method to close the database session."""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
