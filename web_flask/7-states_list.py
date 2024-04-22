70a83a3b3c6939966ac19fece00fdf975f3e525d
Sanaalagh <Sanaalaghzou@gmail.com>
2024-04-22 03:18:07 -0700

    fix issue

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+    Sript that starts a Flask web application
 """
-from models.state import State
-from models.city import City
 from flask import Flask, render_template
 from models import storage
 app = Flask(__name__)
@@ -25,6 +23,5 @@ def state_list():
     states = storage.all('State').values()
     return render_template("7-states_list.html", states=states)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)
