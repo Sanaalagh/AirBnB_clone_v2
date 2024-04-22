701e49938020df37e37d1cf7fe6b37b1fdc09acb
Sanaalagh <Sanaalaghzou@gmail.com>
2024-04-22 04:03:39 -0700

    fixing issue

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,8 +17,9 @@ def teardown_db(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page with a list of all State objects"""
-    states = storage.all(State).values()
+    states = list(storage.all(State).values())
     states_sorted = sorted(states, key=lambda x: x.name)
+    print(f"States found: {len(states_sorted)}")  # Debug: Check how many states are fetched
     return render_template('7-states_list.html', states=states_sorted)
 
 
